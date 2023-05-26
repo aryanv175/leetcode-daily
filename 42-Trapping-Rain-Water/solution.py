deab5f9f8dd9c4401193ff79a626a78ef48478a9
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-05-26 04:31:45 +0200

    Update solution.py

--- 42-Trapping-Rain-Water/solution.py
+++ 42-Trapping-Rain-Water/solution.py
@@ -11,3 +11,46 @@ class Solution:
         return water # return!
 
 # this problem took a while, these are the two solutions i tried before getting to the correct one.
+
+        '''water = 0
+        curr = 0
+        while curr < len(h)-2:
+            nex = curr+2
+            if h[curr] == 0 or nex == len(h):
+                curr += 1 
+                continue
+            if h[curr] > max(h[curr+1:]):
+                if h[curr+1] < max(h[curr+2:]):
+                    while h[curr+1] > h[nex] and nex < len(h):
+                        nex+= 1
+                    water += (nex - curr - 1)*min(h[curr], h[nex])
+                    for i in range(curr+1, nex):
+                        water -= h[i]
+                    curr = nex 
+                    continue
+                else:
+                    curr+= 1
+                    continue
+            while h[curr] > h[nex]:
+                nex+= 1      
+            water += (nex - curr - 1)*min(h[curr], h[nex])
+            for i in range(curr+1, nex):
+                water -= h[i]
+            curr = nex
+            print(water)
+        if water<0:
+            return 0
+        return water
+        water = 0
+        curr = 1
+        while curr < len(h) - 2:
+            nex = curr + 2
+
+            if h[curr] < h[curr + 1] or h[curr + 1]> h[nex]:
+                curr += 1
+                continue
+            water += min(h[curr], h[nex]) - h[curr + 1]
+            print(h[curr], h[nex], water)
+            curr = nex
+        return water'''
+        
