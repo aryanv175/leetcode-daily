5d32b3cb63872151c3d558c34e15f3c50743d4b1
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-05-26 04:31:06 +0200

    Create solution.py

---
+++ 42-Trapping-Rain-Water/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trap(self, h: List[int]) -> int:
+
+        water = 0 # var to store the amount of water
+        if max(h) == min(h): # this is to increase efficiency for an edge case. [1]*(10**4). If the min and max are equal then water trapped = 0
+            return  0
+        for i in range(1, len(h)-1): # not considering the first and last spot as no water can be stored there
+            temp = min(max(h[:i]), max(h[i+1:])) - h[i] # this is store the value of water possible
+            if temp > 0: # only add it if it nets out to be positive
+                water += temp 
+        return water # return!
